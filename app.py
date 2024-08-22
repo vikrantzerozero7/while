9a9571f107851707a51e47efe88693505a09994d
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-22 15:57:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
-
-def store_value(key):
-    st.session_state[key] = st.session_state["_"+key]
-def load_value(key):
-    st.session_state["_"+key] = st.session_state[key]
-
-load_value("my_key")
-st.number_input("Number of filters", key="_my_key", on_change=store_value, args=["my_key"])
+while True:
+    st.session_state.key = None
+    query = st.text_input("enter query")
+    st.write(query)
