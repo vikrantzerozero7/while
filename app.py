5a035e576076b0b6b8cf218c54f26041a9c0711a
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-22 21:41:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ widget_id = (id for id in range(1, 100_00))
 for image in image_file:
     # Do things here
 
-    if(query = st.text_input('Predicted', key=next(widget_id):
-        st.write(qwery)
+    if(st.text_input('Predicted', key=next(widget_id):
+       
