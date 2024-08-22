fa30aa2e067a32aec320e53c5d7e7de37e0907ef
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-22 22:25:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 from module import x  # Importing x from module.py
 
 if x == 3:
-    st.write("zone")
+    x= st.text_input("enter")
+    st.write(x)
 else:
     st.write("x is not 3")
