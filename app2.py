b96f595d47cdd1ef030e947cae577b878dbdc881
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-22 22:42:36 +0530

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,22 @@
+def main():
+    if upload =="upload_successful":
+        query = st.text_input("Ask Question")
+        # prompt: get question and answer part
+        if st.button('Submit Answer'):
+            result = chain.invoke(query)
+        
+        if "answer is not available in the context" in result:
+            st.write("No answer")
+        else:
+            st.write(result)
+            docs1 = vector_store.similarity_search(query,k=3)
+            data_dict = docs1[0].metadata
+            st.write("\nBook Name : ",data_dict["Book name"])
+            st.write("Chapter : ",data_dict["Chapter"])
+            st.write("Title : ",data_dict["Topic"])
+            st.write("Subtopic : ",data_dict["Subtopic"])
+            st.write("Subsubtopic : ",data_dict["Subsubtopic"])
+    
+
+if __name__=='__main__':
+    main()
