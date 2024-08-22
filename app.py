b97f6a02fee78ddc0a9711720e226076b4847ad9
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-22 16:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import streamlit as st
+= import streamlit as st
 import time
 
 # Generate a dynamic key based on the current time
 while True:
   st.session_state.clear()
-  query = st.text_input("Enter query")
+  key = 1
+  query = st.text_input("Enter query", key =  key)
+  key = key + 1
 
   st.write(query)
   st.session_state.clear()
