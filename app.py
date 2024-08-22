a59cd95578702f49beaac044d3f3a137e911d719
vikrantzerozero7 <85125735+vikrantzerozero7@users.noreply.github.com>
2024-08-22 15:47:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def store_value():
 
 # Copy the saved value to the temporary key
 st.session_state["_my_key"] = st.session_state["my_key"]
-st.number_input("Number of filters", key="_my_key", on_change=store_v
+st.number_input("Number of filters", key="_my_key", on_change=store_value)
